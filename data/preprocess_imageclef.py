944d2770d7daab615fb05869e89efabb740f2ecf
Edward <anqiyuanms@hotmail.com>
2021-09-26 22:33:53 -0500

    parse_box_feat()

--- data/preprocess_imageclef.py
+++ data/preprocess_imageclef.py
@@ -202,10 +202,10 @@ def process_answers(q):
 
 
 if __name__ == '__main__':
-    # parse_box_feat()
+    parse_box_feat()
     # get_qa_pairs()  # run once
-    process_text()
-    tokenize_questions()
-    t = json.load(open('vqa_imageclef_toked.json'))
-    process_questions(t)
-    process_answers(t)
+    # process_text()
+    # tokenize_questions()
+    # t = json.load(open('vqa_imageclef_toked.json'))
+    # process_questions(t)
+    # process_answers(t)
