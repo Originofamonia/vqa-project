59454cfcedab3549c5809f76fc8db9cb9b3e8c7f
Edward <anqiyuanms@hotmail.com>
2021-10-21 15:44:07 -0500

    n_obj = 16

---
+++ plot.py
@@ -0,0 +1,21 @@
+"""
+Visualize:
+1. original image
+2. plot all boxes with central points
+3. plot winning box and its neighbors
+"""
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+import pandas as pd
+import os
+import glob
+import cv2
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
