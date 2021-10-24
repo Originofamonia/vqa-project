58c3b503b0a6a01582a287d07ab439e04114b88d
Edward <anqiyuanms@hotmail.com>
2021-10-24 16:55:44 -0500

    edges_sorted, edges_ind = torch.sort(edges, descending=True)

--- plot.py
+++ plot.py
@@ -209,7 +209,7 @@ def save_plot_nodes():
             # count_sort_ind = np.argsort(-count)
             # h_max_boxes = boxes[h_max_idx[count_sort_ind][:10]]
             edges = topm[j].flatten()
-            edges_sorted, edges_ind = torch.sort(edges)
+            edges_sorted, edges_ind = torch.sort(edges, descending=True)
 
             f2 = os.path.join(args.plot_dir, f"{iid.strip('.jpg')}_h_max.jpg")
             plot_connect_lines(mosaic, h_max_boxes, f2, color=None, line_thickness=None)
