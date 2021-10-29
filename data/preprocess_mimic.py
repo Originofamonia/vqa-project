3ee4a3c64999611a6b9207a788fc528f1f4a43fa
Edward <anqiyuanms@hotmail.com>
2021-10-28 22:50:08 -0500

    for ans in row:

--- data/preprocess_mimic.py
+++ data/preprocess_mimic.py
@@ -220,7 +220,7 @@ def process_questions(q):
 def process_answers(q):
     counts = {}
     for row in q:
-        for ans, c in row:
+        for ans in row:
             counts[ans] = counts.get(ans, 0) + 1
 
     cw = sorted([(count, w) for w, count in counts.items()], reverse=True)
