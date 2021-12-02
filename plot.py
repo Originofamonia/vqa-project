832630f721494ba7e80d5a8209158c0cb57f2324
Edward <anqiyuanms@hotmail.com>
2021-12-02 16:18:20 -0600

    # f"{iid},"

--- plot.py
+++ plot.py
@@ -433,7 +433,7 @@ def plot_given_fig():
     oix = oix.cpu().numpy()
     iid = dataset.vqa[idx]['image_id']
     results.append(
-        f"{iid},"
+        # f"{iid},"
         f"{dataset.vqa[idx]['question']},"
         f"{dataset.a_itow[oix]},"
         f"{dataset.vqa[idx]['answer']}"
