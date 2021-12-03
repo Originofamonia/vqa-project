cc8892462fc90bcc07d44f3587942f6a85e28603
Edward <anqiyuanms@hotmail.com>
2021-12-03 01:20:29 -0600

    question = 'How many arms are in the picture?'     iid = '67765'

--- plot.py
+++ plot.py
@@ -399,8 +399,8 @@ def plot_given_fig():
 
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_54.17.pt')
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
-    question = 'Are these men twins?'
-    iid = '196853'
+    question = 'How many arms are in the picture?'
+    iid = '67765'
     test_batch = get_iid_from_question(dataset, question, iid)
     # test_sampler = SequentialSampler(dataset)
     # loader_test = DataLoader(dataset, batch_size=args.bsize,
