3e6468df1bce00894c13748dff9519a9f9e6f650
Edward <anqiyuanms@hotmail.com>
2021-12-02 21:51:48 -0600

    question = 'What is the room?'

--- plot.py
+++ plot.py
@@ -399,7 +399,7 @@ def plot_given_fig():
 
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_54.17.pt')
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
-    question = 'Is this a prison toilet?'
+    question = 'What is the room?'
     iid = '325114'
     test_batch = get_iid_from_question(dataset, question, iid)
     # test_sampler = SequentialSampler(dataset)
