e461fbe9aca81382fe42c7a8aaaa8e14a82f1cff
Edward <anqiyuanms@hotmail.com>
2021-12-03 01:26:15 -0600

    question = 'Is this healthy?'     iid = '192458'

--- plot.py
+++ plot.py
@@ -399,8 +399,8 @@ def plot_given_fig():
 
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_54.17.pt')
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
-    question = 'What animals are on the bed?'
-    iid = '71226'
+    question = 'Is this healthy?'
+    iid = '192458'
     test_batch = get_iid_from_question(dataset, question, iid)
     # test_sampler = SequentialSampler(dataset)
     # loader_test = DataLoader(dataset, batch_size=args.bsize,
