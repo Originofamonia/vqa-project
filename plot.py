7fc1ae69ef543d0eb55a139a2adb24702f91f406
Edward <anqiyuanms@hotmail.com>
2021-12-02 21:41:15 -0600

    question = 'Is this a prison toilet?'     iid = '325114'

--- plot.py
+++ plot.py
@@ -399,8 +399,8 @@ def plot_given_fig():
 
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_54.17.pt')
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
-    question = 'What is behind the skier?'
-    iid = '88507'
+    question = 'Is this a prison toilet?'
+    iid = '325114'
     test_batch = get_iid_from_question(dataset, question, iid)
     # test_sampler = SequentialSampler(dataset)
     # loader_test = DataLoader(dataset, batch_size=args.bsize,
