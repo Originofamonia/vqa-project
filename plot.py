a1e4a9ca42f342bcdbd6e274a34e21fecb491201
Edward <anqiyuanms@hotmail.com>
2022-01-08 20:23:11 -0600

    iid = '88507'

--- plot.py
+++ plot.py
@@ -398,7 +398,7 @@ def plot_given_fig():
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_54.17.pt')
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
     question = 'Are there any trees in this picture?'
-    iid = '088507'
+    iid = '88507'
     test_batch = get_iid_from_question(dataset, question, iid)
     # test_sampler = SequentialSampler(dataset)
     # loader_test = DataLoader(dataset, batch_size=args.bsize,
