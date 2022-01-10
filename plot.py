d7a174afcbf16e824f6d9804127e54305fd4f64e
Edward <anqiyuanms@hotmail.com>
2022-01-09 23:21:48 -0600

    question = 'Is this a prison toilet?'

--- plot.py
+++ plot.py
@@ -403,7 +403,7 @@ def plot_given_fig():
     model_file = os.path.join(args.save_dir, 'vqa_36_8_16_83.44.pt')
     # model_file = os.path.join(args.save_dir, 'model_38.pth.tar')  # xinyue's model
     dataset = VQA_Dataset(args.data_dir, args.emb, train=False)
-    question = 'What is the room?'
+    question = 'Is this a prison toilet?'
     iid = '325114'
     test_batch = get_iid_from_question(dataset, question, iid)
 
